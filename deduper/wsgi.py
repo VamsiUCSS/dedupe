99dafd171bfd963c8064b4b3a2e1499915f5f4ec
Chandra S S Vamsi <ucssvamsi@gmail.com>
2016-09-18 16:34:37 +0530

    Project rename

--- deduper/wsgi.py
+++ deduper/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dedupe project.
+WSGI config for deduper project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dedupe.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deduper.settings")
 
 application = get_wsgi_application()
--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-"""dedupe URL Configuration
+"""deduper URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
