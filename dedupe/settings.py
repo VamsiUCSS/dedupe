049afec5a6268a54fc82f259e5420793adc9eea4
Chandra S S Vamsi <ucssvamsi@gmail.com>
2016-09-18 16:04:07 +0530

    Renamed folder

--- dedupe/settings.py
+++ dedupe/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'makeathon.urls'
+ROOT_URLCONF = 'dedupe.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'makeathon.wsgi.application'
+WSGI_APPLICATION = 'dedupe.wsgi.application'
 
 
 # Database
